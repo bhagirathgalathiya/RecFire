cf4eae8d21d242b465d8cd5e115efffa9edc3a86
bhagirathgalathiya <59164325+bhagirathgalathiya@users.noreply.github.com>
2020-10-15 11:40:44 +0530

    Update MainActivity.java

--- app/src/main/java/com/bhagi/recfire/MainActivity.java
+++ app/src/main/java/com/bhagi/recfire/MainActivity.java
@@ -32,29 +32,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseRecyclerAdapter<hospital,BlogAdapter>  FirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<hospital, BlogAdapter>
-                (hospital.class, R.layout.custom_list,BlogAdapter.class ,mDatabase) {
-            @Override
-            protected void populateViewHolder(BlogAdapter blogAdapter, final hospital hospi, final int i) {
-                final DatabaseReference postRef = getRef(i);
-
-                blogAdapter.setH_name(hospi.getH_name());
-                blogAdapter.setImg(hospi.getH_pgflag());
-
-                final String postKey = postRef.getKey();
-
-                blogAdapter.mView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-
-                        Intent intent = new Intent(MainActivity.this,Welcome.class);
-                        intent.putExtra("id",postKey);
-                        startActivity(intent);
-                    }
-                });
-            }
-        };
+        
+        #code
+            
+            
         recyclerView.setAdapter(FirebaseRecyclerAdapter);
     }
 
